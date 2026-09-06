93e307d42680106d3bb5c8d2d40cf6d00fa34b9d
johanfredrikholmberg-del <johanfredrikholmberg@gmail.com>
2026-09-06 21:06:43 +0200

    Load compact studies mobile UI

--- studielots-runtime-v625.js
+++ studielots-runtime-v625.js
@@ -1,5 +1,5 @@
 (()=>{
 'use strict';
 function load(src,done){const s=document.createElement('script');s.src=src;s.async=false;s.onload=()=>done&&done();s.onerror=()=>console.error('StudieLots kunde inte ladda',src);document.body.appendChild(s)}
-load('/studielots-v624.js?v=624',()=>load('/studielots-runtime-overlay-v706.js?v=706'));
+load('/studielots-v624.js?v=624',()=>load('/studielots-runtime-overlay-v706.js?v=706',()=>load('/studielots-studies-compact-v707.js?v=707')));
 })();
