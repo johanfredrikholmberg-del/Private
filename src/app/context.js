8efd8f1e6a4feb8dd8245a4e9228b5baec0a21a5
johanfredrikholmberg-del <johanfredrikholmberg@gmail.com>
2026-09-12 20:46:14 +0200

    Remove unused context scaffold

--- src/app/context.js
+++
@@ -1,9 +0,0 @@
-(()=>{'use strict';
-const root=window.StudieLotsV2=window.StudieLotsV2||{};
-const state={page:'home',route:'ordinary',source:'ladok',courses:[],selectedOpportunity:null,plannerData:null,selectedUniversity:'',opportunityLevel:'Grundnivå'};
-const q=s=>document.querySelector(s);
-const qa=s=>[...document.querySelectorAll(s)];
-const fmt=n=>Number(n||0).toLocaleString('sv-SE',{maximumFractionDigits:1});
-const esc=s=>String(s??'').replace(/[&<>"']/g,m=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#039;'}[m]));
-root.appContext=Object.freeze({state,q,qa,fmt,esc});
-})();
